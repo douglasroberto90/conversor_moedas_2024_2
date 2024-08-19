1c82061acde7ec472a9ec1c95bcdce0e2b791e6a
douglasroberto90 <douglas_roberto90@hotmail.com>
2024-08-19 17:32:21 -0300

    Versão finalizada

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:conversor_moedas_2024_2/repositories/repository.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController controllerReal = TextEditingController();
+  TextEditingController controllerDolar = TextEditingController();
+  TextEditingController controllerEuro = TextEditingController();
+
+  late double cotacaoDolar;
+  late double cotacaoEuro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,19 +26,116 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Icon(Icons.monetization_on, size: 200,),
-            TextField(
-              decoration: InputDecoration(label: Text("Real R\$")),
+            Icon(
+              Icons.monetization_on,
+              size: 200,
             ),
-            TextField(
-              decoration: InputDecoration(label: Text("Dolar US\$")),
-            ),
-            TextField(
-              decoration: InputDecoration(label: Text("Euro €")),
-            )
+            FutureBuilder(
+                future: Repository.pegarDados(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(
+                          child: Text(
+                        "Aguardando dados",
+                        style: TextStyle(fontSize: 20),
+                      ));
+                    default:
+                      if (snapshot.hasError) {
+                        return Center(
+                            child: Text(
+                          "Ocorreu um erro",
+                          style: TextStyle(fontSize: 20),
+                        ));
+                      } else {
+                        cotacaoDolar = snapshot.data?["results"]["currencies"]
+                            ["USD"]["buy"];
+                        cotacaoEuro = snapshot.data?["results"]["currencies"]
+                            ["EUR"]["buy"];
+                        return Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                              child: TextField(
+                                controller: controllerReal,
+                                decoration: InputDecoration(
+                                    label: Text(
+                                      "Real",
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                    prefix: Text("R\$ ")),
+                                keyboardType: TextInputType.number,
+                                onChanged: _realAlterado,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                              child: TextField(
+                                controller: controllerDolar,
+                                decoration: InputDecoration(
+                                    label: Text("Dolar",
+                                        style: TextStyle(fontSize: 20)),
+                                    prefix: Text("US\$ ")),
+                                keyboardType: TextInputType.number,
+                                onChanged: _dolarAlterado,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                              child: TextField(
+                                controller: controllerEuro,
+                                decoration: InputDecoration(
+                                    label: Text("Euro",
+                                        style: TextStyle(fontSize: 20)),
+                                    prefix: Text("€ ")),
+                                keyboardType: TextInputType.number,
+                                onChanged: _euroAlterado,
+                              ),
+                            ),
+                          ],
+                        );
+                      }
+                  }
+                })
           ],
         ),
       ),
     );
+  }
+
+  void _realAlterado(String texto) {
+    if (texto.isEmpty){
+      _limparCampos();
+    }
+    double valorCampoReal = double.parse(controllerReal.text);
+    controllerDolar.text = (valorCampoReal / cotacaoDolar).toStringAsFixed(2);
+    controllerEuro.text = (valorCampoReal / cotacaoEuro).toStringAsFixed(2);
+  }
+
+  void _dolarAlterado(String texto) {
+    if (texto.isEmpty){
+      _limparCampos();
+    }
+    double valorCampoDolar = double.parse(texto);
+    controllerReal.text = (valorCampoDolar * cotacaoDolar).toStringAsFixed(2);
+    controllerEuro.text =
+        ((valorCampoDolar * cotacaoDolar) / cotacaoEuro).toStringAsFixed(2);
+  }
+
+  void _euroAlterado(String texto) {
+    if (texto.isEmpty){
+      _limparCampos();
+    }
+    double valorCampoEuro = double.parse(texto);
+    controllerReal.text = (valorCampoEuro * cotacaoEuro).toStringAsFixed(2);
+    controllerDolar.text =
+        ((valorCampoEuro * cotacaoEuro) / cotacaoDolar).toStringAsFixed(2);
+  }
+
+  void _limparCampos(){
+    controllerReal.clear();
+    controllerDolar.clear();
+    controllerEuro.clear();
   }
 }
--- lib/repositories/repository.dart
+++ lib/repositories/repository.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class Repository {
-
-void pegarDados(){
-  Uri url = Uri.parse('https://api.hgbrasil.com/finance?key=86c89543');
-  final resposta = http.get(url);
-}
+  static Future<Map<String,dynamic>> pegarDados() async {
+    await dotenv.load(fileName: "keys.env");
+    Uri url = Uri.parse('https://api.hgbrasil.com/finance?key=${dotenv.env['API_KEY']}');
+    final resposta = await http.get(url);
+    return jsonDecode(resposta.body);
+  }
 }
